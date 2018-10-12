8e40ede8302d36eac89c21a12340a55eabd7bcf5
Guillaume VanderEst <guillaume@vanderest.org>
2018-10-11 23:25:22 -0700

    feat(arrayToggle) create arrayToggle

---
+++ arrayToggle.ts
@@ -0,0 +1,15 @@
+/**
+ * Return an Array with a value included/removed, with optional forcing.
+ *
+ * @param arr Array of values to be toggled
+ * @param val Value being toggled
+ * @param forced If true, it will force the item to be included, otherwise, remove
+ * @returns Array with value toggled
+ */
+export default function arrayToggle<T>(arr: T[], val: T, forced?: boolean): T[] {
+    if (arr.includes(val) || forced === false) {
+        return arr.filter((v: typeof val) => v !== val);
+    }
+    return [...arr, val];
+}
+
